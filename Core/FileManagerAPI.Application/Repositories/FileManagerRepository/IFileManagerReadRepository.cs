78976c689cdc24f6a16aa50d1c6230bec2bf31bc
ezizdemirov <eziz.demirov84@mail.com>
2022-05-11 09:36:32 +0400

    x

---
+++ Core/FileManagerAPI.Application/Repositories/FileManagerRepository/IFileManagerReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManagerAPI.Domain.Entities;
+
+namespace FileManagerAPI.Application.Repositories.FileManagerRepository
+{
+    public  interface IFileManagerReadRepository : IReadRepository<FileManager>
+    {
+
+    }
+}
---
+++ Infrastructure/FileManagerAPI.Persistence/Repositories/FileManagerReadRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManagerAPI.Application.Repositories.FileManagerRepository;
+using FileManagerAPI.Domain.Entities;
+using FileManagerAPI.Persistence.Contexts;
+
+namespace FileManagerAPI.Persistence.Repositories
+{
+   public class FileManagerReadRepository:ReadRepository<FileManager>, IFileManagerReadRepository
+    {
+        public FileManagerReadRepository(FileManagerAPIDbContext context) : base(context)
+        {
+            
+        }
+    }
+}
