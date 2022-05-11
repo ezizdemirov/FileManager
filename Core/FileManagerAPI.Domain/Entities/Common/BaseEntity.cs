287a359a415af65a103a28cf7ba19402d70ce980
ezizdemirov <eziz.demirov84@mail.com>
2022-05-11 13:11:54 +0400

    cc

--- Core/FileManagerAPI.Domain/Entities/Common/BaseEntity.cs
+++ Core/FileManagerAPI.Domain/Entities/Common/BaseEntity.cs
@@ -8,6 +8,7 @@ namespace FileManagerAPI.Domain.Entities.Common
 {
   public  class BaseEntity
     {
+        //
         public int Id { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
